54f3ed93f8da69cfb58e7954887bd68fb0db7ccc
right <right@gamepc>
2024-10-31 23:50:44 +0900

    Start

---
+++ PFSite/src/main/java/com/example/demo/PfSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfSiteApplication.class, args);
+	}
+
+}
