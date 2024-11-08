a6d46f2c4529aacafe45c81e39bb611a99d27594
right <right@gamepc>
2024-11-09 06:07:45 +0900

    トップページ

--- PFSite/src/main/java/com/example/demo/controller/ContactController.java
+++ PFSite/src/main/java/com/example/demo/controller/ContactController.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.example.demo.EchoForm;
 import com.example.demo.model.ContactModel;
 
 @Controller
@@ -21,6 +24,17 @@ public class ContactController {
 		mav.addObject("rb",rb);
 		mav.setViewName("content/confirmContact");
 		return mav;
+	}
+	
+	@RequestMapping("echo")
+	public class EchoController{
+		
+		@RequestMapping(method = RequestMethod.GET)
+		public String viewInput(Model model) {
+			EchoForm form = new EchoForm();
+			model.addAttribute(form);
+			return "echo/input";
+		}
 	}
 
 }
